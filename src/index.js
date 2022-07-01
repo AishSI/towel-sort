ed16c14699167084e78d748afb1a636eceb86e8c
AishSI <aishsi@gmail.com>
2022-07-01 22:35:57 +0700

    refactor: del extra loop

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@
 module.exports = function towelSort(matrix) {
    if (typeof matrix !== "undefined") {
       for (let i = 0; i < matrix.length; i++) {
-         for (let j = 0; j < matrix[i].length; j++) {
-            i % 2 ? matrix[i].sort((a, b) => (b - a)) : matrix[i].sort((a, b) => (a - b));
-         }
+         i % 2 ? matrix[i].sort((a, b) => (b - a)) : matrix[i].sort((a, b) => (a - b));
       }
       return [].concat(...matrix);
    } else {
